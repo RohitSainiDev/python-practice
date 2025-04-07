a383a504708df0af89a8ba597c97861242227954
Rohit Saini <emailrohitsaini@gmail.com>
2025-04-07 17:01:00 +0530

    Add arithmetic operators and math functions demonstration scripts

---
+++ 06_arithmetic_operators.py
@@ -0,0 +1,29 @@
+# Arithmetic operators are used to perform arithmetic operations on numbers.
+
+number_of_offers = 00
+
+number_of_offers = number_of_offers + 1
+# number_of_offers += 1
+print(number_of_offers)
+
+number_of_offers = number_of_offers - 1
+# number_of_offers -= 1
+print(number_of_offers)
+
+number_of_offers = 1
+number_of_offers = number_of_offers * 2
+# number_of_offers *= 2
+print(number_of_offers)
+
+number_of_offers = number_of_offers / 2
+# number_of_offers /= 2
+print(number_of_offers)
+
+number_of_offers = 2
+number_of_offers= number_of_offers ** 2            
+# number_of_offers **= 2
+print(number_of_offers)
+
+number_of_offers = number_of_offers % 2
+# number_of_offers %= 2
+print(number_of_offers)
---
+++ 07_math_functions.py
@@ -0,0 +1,14 @@
+import math
+
+int_num = 5
+float_num = 5.5
+negative_int =-4
+
+print(f"Absolute function: abs({negative_int}) = {abs(negative_int)}")
+print(f"Power funtion: pow({int_num},2) = {pow(int_num, 2)}")
+print(f"Sqrt function: math.sqrt({int_num}) = {math.sqrt(int_num)}")
+print(f"Floor function: math.floor({float_num})= {math.floor(float_num)}")
+print(f"Ceil function: math.ceil({float_num}) = {math.ceil(float_num)}")
+print(f"Pi = {math.pi}")
+print(f"E = {math.e}")
+print(f"Power function: pow({int_num},2) = {pow(int_num, 2)}")
