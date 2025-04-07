85b7355fe2e901f3282d78e1d211c21e9505bd21
Rohit Saini <emailrohitsaini@gmail.com>
2025-04-07 12:24:44 +0530

    Add program to calculate the area of a rectangle

---
+++ 04_area_of_rectangle.py
@@ -0,0 +1,8 @@
+# Write a program to calculate the area of a rectangle.
+
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+
+area_of_rectangle = length*breadth
+
+print(f"Area of the rectangle is: {area_of_rectangle} sq. units")
