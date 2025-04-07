b24794365af4af2f7106d11210eedd8b8b5ef8bd
Rohit Saini <emailrohitsaini@gmail.com>
2025-04-07 17:55:42 +0530

    Enhance math functions script and add area calculation for a circle

--- 007_math_functions.py
+++ 007_math_functions.py
@@ -1,3 +1,4 @@
+# Python has a built-in math module that provides various mathematical functions.
 import math
 
 int_num = 5
@@ -5,10 +6,12 @@ float_num = 5.5
 negative_int =-4
 
 print(f"Absolute function: abs({negative_int}) = {abs(negative_int)}")
+print(f"Round function: round({float_num}) = {round(float_num)}")
 print(f"Power funtion: pow({int_num},2) = {pow(int_num, 2)}")
+print(f"Max function: max({int_num}, {float_num},{negative_int}) = {max(int_num, float_num,negative_int)}")
+print(f"Min function: min({int_num}, {float_num},{negative_int}) = {min(int_num, float_num,negative_int)}")
+print(f"Pi = {math.pi}")
+print(f"E = {math.e}")
 print(f"Sqrt function: math.sqrt({int_num}) = {math.sqrt(int_num)}")
 print(f"Floor function: math.floor({float_num})= {math.floor(float_num)}")
 print(f"Ceil function: math.ceil({float_num}) = {math.ceil(float_num)}")
-print(f"Pi = {math.pi}")
-print(f"E = {math.e}")
-print(f"Power function: pow({int_num},2) = {pow(int_num, 2)}")
--- 008_circumference_of_circle.py
+++ 008_circumference_of_circle.py
@@ -2,5 +2,5 @@ import math
 
 radius = float(input("Enter the radius of the circle: "))
 circumference = 2 * math.pi * radius
-print("The circumference of the circle is:", round(circumference,2))
+print(f"The circumference of the circle is: {round(circumference,2)} unit")
 
