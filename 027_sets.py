2f37830b950937679ef43093cd16b452516bcca1
Rohit Saini <emailrohitsaini@gmail.com>
2025-04-12 17:18:09 +0530

    feat: add initial implementation of sets in Python

---
+++ 027_sets.py
@@ -0,0 +1,13 @@
+fruits = {"apple", "banana", "orange", "coconut"}
+print(fruits)
+# print(fruits[0])  # Error
+
+print(len(fruits))
+
+print("pineapple" in fruits)
+fruits.add("pineapple")
+fruits.remove("apple")
+fruits.pop()
+fruits.clear()
+
+print(fruits)
