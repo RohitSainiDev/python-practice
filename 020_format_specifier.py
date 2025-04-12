2750d5772ccf935874f22aa98b92a613c5dc2093
Rohit Saini <emailrohitsaini@gmail.com>
2025-04-12 08:37:43 +0530

    add example script demonstrating Python format specifiers

---
+++ 020_format_specifier.py
@@ -0,0 +1,12 @@
+price_1 = 3.1415
+price_2 = -9184.567
+price_3 = 1200.44
+
+print(f"{price_1:.2f}")
+print(f"{price_1:10}")
+print(f"{price_1:010}")
+print(f"{price_1:+}")
+print(f"{price_1:,}")
+print(f"{price_1:<10}")
+print(f"{price_1:>10}")
+print(f"{price_1:^10}")
